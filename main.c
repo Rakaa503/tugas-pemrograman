e0e83718f3f6529e3ad50608a0d9f17a3e509c23
Rakaa503 <azmyervawanrakhaavilla@gmail.com>
2024-09-23 13:11:22 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+
+    
+    
+    
+    
+    
+
+    int main ()
+    {
+        
+     
+          printf ("Data Mahasiswa STT MANDALA\n");
+          printf ("Nama     :  Rakha avilla azmy ervawan\n"); 
+          printf ("NIM      :  2441078\n");
+		  printf ("Jurusan  :  Teknik informatika\n");
+		  
+		  return 0; 
+		  
+	}
