3a22daf585451974f9d741fa05b2111788fb672a
Rakaa503 <azmyervawanrakhaavilla@gmail.com>
2024-09-25 19:53:14 +0700

    Add files via upload

---
+++ main.c1.cpp
@@ -0,0 +1,24 @@
+       #include <stdio.h>
+       
+       int main() {
+       	//  Declare the number and result 
+       	float panjang = 10.0; //  First number
+       	float lebar   = 7.0;  //  second number 
+       	float luas;       // Variable to hold the result 
+       	
+       	
+        //Show  what ' s going on 
+        printf("Panjang: %.2f cm\n",panjang);
+    
+        	
+       	
+	     // perform the multiplication 
+	     luas = panjang  * lebar;
+	     
+	     // Display the result
+	     printf("Luas persegi panjang adalah : %.2f  cm\n", luas);
+	     
+	     
+	     return 0;
+}
+ 
