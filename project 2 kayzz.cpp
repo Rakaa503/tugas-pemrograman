7bdf262cf1bc844b303c253c739d56137cc66f43
Rakha avilla Azmy ervawan 2441078 <azmyervawanrakhaavilla@gmail.com>
2024-09-30 13:24:20 +0700

    Add files via upload

---
+++ project 2 kayzz.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14
+
+
+
+        int main()
+        {
+                float diameter = 15.0;
+                float jari_jari = diameter / 2.0;
+                float volume;
+                
+                
+                
+                printf ("sebuah bola dengan diameter %.2f cm memiliki volume sebesar %.2f cm3", diameter, volume);
+                
+                
+                return 0;
+}
