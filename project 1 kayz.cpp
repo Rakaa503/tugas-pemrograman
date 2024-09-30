1d87a47c92fe89d85c4710f2b99b123f5cdadbe1
Rakaa503 <azmyervawanrakhaavilla@gmail.com>
2024-09-30 13:02:45 +0700

    Add files via upload

---
+++ project 1 kayz.cpp
@@ -0,0 +1,19 @@
+       #include <stdio.h>
+       #include <stdlib.h>
+
+    
+       int main()
+       
+       {
+                float alas = 8.0;
+                float tinggi = 5.0;
+                float luas;
+                
+                luas = 0.5 * alas * tinggi;
+                
+                
+                
+                
+                
+                return 0;
+}
