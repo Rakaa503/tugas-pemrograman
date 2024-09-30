c728f71e427f57f7165b5be0d5e0f13341fd2016
Rakaa503 <azmyervawanrakhaavilla@gmail.com>
2024-09-30 12:21:09 +0700

    Add files via upload

---
+++ latihan kayzzz.cpp no 3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+   char nim[10];
+   char nama[30];
+   int nilai;
+   
+    
+   
+   
+      
+    printf("NIM : %S", 2441078);
+    printf("NAMA : %s", nama);
+    printf("NILAI : %i", 85);
+    
+    return 0;
+}
