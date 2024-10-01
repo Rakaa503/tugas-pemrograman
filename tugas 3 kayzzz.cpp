5b2a11f03b39a2ba02a5d65d910bfdbdc2b9b9dd
Rakha avilla Azmy ervawan 2441078 <azmyervawanrakhaavilla@gmail.com>
2024-10-01 12:51:24 +0700

    Add files via upload

---
+++ tugas 3 kayzzz.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+    int main()
+    {
+	
+	         float alas = 4.0;
+	         float tinggi = 5.0;
+	         float sisi_miring;
+	         
+	         
+	
+	         
+	         
+	         
+	         
+	              return 0;
+}
