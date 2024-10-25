5038f45d77eb2f5ea5f713c5c909418d13a11c53
Rakha avilla Azmy ervawan 2441078 <azmyervawanrakhaavilla@gmail.com>
2024-10-25 19:22:07 +0700

    Add files via upload

---
+++ rakha avilla.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int bilangan1, bilangan2, hasil;
+
+  printf("Masukkan bilangan pertama: ");
+  scanf("%d", &bilangan1);
+
+  printf("Masukkan bilangan kedua: ");
+  scanf("%d", &bilangan2);
+
+  hasil = bilangan1 + bilangan2;
+
+  printf("Hasil penjumlahan: %d\n", hasireturn 0;  
+}
